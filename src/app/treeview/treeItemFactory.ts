1e4f4f5c758b28227e96537bce0ad52902fbf708
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2026-01-29 00:05:14 +0100

    create tree items from band

--- src/app/treeview/treeItemFactory.ts
+++ src/app/treeview/treeItemFactory.ts
@@ -5,33 +5,92 @@ import type { TreeItem } from './treeItem';
 
 export class TreeItemFactory {
   static fromBand(band: Band, queryCountMap: QueryCountMap): TreeItem {
-    const children = band.metadata.artistNames.map((artist) => {
-      const count = queryCountMap.get(artist) || 0;
-      const label = createQueryCountString(artist, count);
-      const artistChildren: TreeItem[] = [];
+    const children: TreeItem[] = [];
 
-      band.metadata.albums.forEach((album) => {
-        if (album.artist.names.includes(artist)) {
-          artistChildren.push({
-            label: album.toString(),
-          });
-        }
-      });
-
-      return {
-        label,
-        open: false,
-        children: artistChildren,
-      } as TreeItem;
-    });
-
-    return {
+    children.push({
       label: createQueryCountString(
-        band.name,
+        'Artist/Releases',
         band.metadata.artistNames.length,
       ),
+      open: true,
+      children: createBandArtistsReleasesTreeItems(band, queryCountMap),
+    });
+
+    children.push(createBandYearsTreeItem(band, queryCountMap));
+    children.push(createBandKeywordsTreeItem(band, queryCountMap));
+
+    return {
+      label: band.name,
       open: true,
       children,
     };
   }
+}
+
+function createBandArtistsReleasesTreeItems(
+  band: Band,
+  queryCountMap: QueryCountMap,
+): TreeItem[] {
+  const children = band.metadata.artistNames.map((artist) => {
+    const count = queryCountMap.get(artist) || 0;
+    const label = createQueryCountString(artist, count);
+    const artistChildren: TreeItem[] = [];
+
+    band.metadata.albums.forEach((album) => {
+      if (album.artist.names.includes(artist)) {
+        artistChildren.push({
+          label: album.toString(),
+          href: album.url.toString(),
+        });
+      }
+    });
+
+    return {
+      label,
+      open: false,
+      children: artistChildren,
+    } as TreeItem;
+  });
+
+  return children;
+}
+
+function createBandYearsTreeItem(
+  band: Band,
+  queryCountMap: QueryCountMap,
+): TreeItem {
+  const children: TreeItem[] = band.metadata.years.map((year) => {
+    const count = queryCountMap.get(year) || 0;
+    const label = createQueryCountString(year.toString(), count);
+
+    return {
+      label,
+    };
+  });
+
+  return {
+    label: createQueryCountString('Years', band.metadata.years.length),
+    open: false,
+    children,
+  };
+}
+
+function createBandKeywordsTreeItem(
+  band: Band,
+  queryCountMap: QueryCountMap,
+): TreeItem {
+  const children: TreeItem[] = band.metadata.keywords.map((keyword) => {
+    const count = queryCountMap.get(keyword) || 0;
+    const label = createQueryCountString(keyword, count);
+
+    return {
+      label,
+    };
+  });
+
+  return {
+    label: createQueryCountString('Keywords', band.metadata.keywords.length),
+    open: false,
+    children,
+  };
 }
