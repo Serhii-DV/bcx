1b5e7e6f2af0bc64bf6af62b1964125c450a383c
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2026-01-28 13:44:03 +0100

    use snippet

--- src/lib/components/bcx/BcxTreeView.svelte
+++ src/lib/components/bcx/BcxTreeView.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-import BcxTreeView from './BcxTreeView.svelte';
 import type { TreeItem } from './types';
 
 interface Props {
@@ -14,31 +13,35 @@ function handleItemClick(item: TreeItem) {
 }
 </script>
 
-{#if items.length > 0}
-  <ol>
-    {#each items as item}
-      <li>
-        {#if item.children && item.children.length > 0}
-          <details bind:open={item.open} class="group">
-            <summary
-              class="cursor-pointer select-none px-2 py-1 hover:bg-white/10 transition-colors"
+{#snippet treeItems(items: TreeItem[])}
+  {#if items.length > 0}
+    <ol>
+      {#each items as item}
+        <li>
+          {#if item.children && item.children.length > 0}
+            <details bind:open={item.open} class="group">
+              <summary
+                class="cursor-pointer select-none px-2 py-1 hover:bg-white/10 transition-colors"
+                onclick={() => handleItemClick(item)}
+              >
+                {item.label}
+              </summary>
+              <ol class="ml-3 mt-1 border-l border-gray-500/50 pl-2">
+                {@render treeItems(item.children)}
+              </ol>
+            </details>
+          {:else}
+            <button
+              class="w-full cursor-pointer text-left px-2 py-1 text-gray-200 hover:bg-white/10 transition-colors"
               onclick={() => handleItemClick(item)}
             >
-              {item.label}
-            </summary>
-            <ol class="ml-3 mt-1 border-l border-gray-500/50 pl-2">
-              <BcxTreeView items={item.children} {onItemClick} />
-            </ol>
-          </details>
-        {:else}
-          <button
-            class="w-full cursor-pointer text-left px-2 py-1 text-gray-200 hover:bg-white/10 transition-colors"
-            onclick={() => handleItemClick(item)}
-          >
-            <span class="ml-2">{item.label}</span>
-          </button>
-        {/if}
-      </li>
-    {/each}
-  </ol>
-{/if}
+              <span class="ml-2">{item.label}</span>
+            </button>
+          {/if}
+        </li>
+      {/each}
+    </ol>
+  {/if}
+{/snippet}
+
+{@render treeItems(items)}
