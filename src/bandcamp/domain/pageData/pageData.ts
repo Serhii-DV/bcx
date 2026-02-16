5a5e8c6d34408d79e949a16e5903b64d7a8fd925
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2026-02-16 14:40:25 +0100

    fix: removed unused files

--- src/bandcamp/domain/pageData/pageData.ts
+++ src/bandcamp/domain/pageData/pageData.ts
@@ -14,21 +14,6 @@ export class BandcampPageData {
     console.log('[BandcampPageData]', this.data);
   }
 
-  /** Returns the album ID if available */
-  get albumId(): number | null {
-    return this.data?.album_id ?? null;
-  }
-
-  /** Optional: returns the track ID */
-  get trackId(): number | null {
-    return this.data?.id ?? null;
-  }
-
-  /** Optional: returns the title */
-  get title(): string | null {
-    return this.data?.title ?? null;
-  }
-
   get fanData(): FanData {
     return detectFanDataFromPageData(this, currentPageUrl);
   }
--- src/bandcamp/domain/track/factory.ts
+++ src/bandcamp/domain/track/factory.ts
@@ -70,7 +70,7 @@ export class TrackFactory {
       ? TrackTime.fromDuration(schema.duration)
       : undefined;
     // albumId is not available in schema, try to get it from pagedata
-    const albumId = bandcampPageData.albumId || undefined;
+    const albumId = bandcampPageData.data?.album_id || undefined;
     const artId =
       (schema.additionalProperty?.find((prop) => prop.name === 'art_id')
         ?.value as number) || 0;
