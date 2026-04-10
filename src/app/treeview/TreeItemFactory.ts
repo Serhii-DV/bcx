d1ef67dfa0a9d751f8d260ccd5378ccfccb034ca
Serhii Diahovchenko <serhii.diahovchenko@gmail.com>
2026-04-11 00:12:51 +0200

    Default textWithQuery query to label when omitted

--- src/app/treeview/TreeItemFactory.ts
+++ src/app/treeview/TreeItemFactory.ts
@@ -14,10 +14,10 @@ export class TreeItemFactory {
     };
   }
 
-  static textWithQuery(label: string, query: string): TreeItem {
+  static textWithQuery(label: string, query?: string): TreeItem {
     return {
       label,
-      query,
+      query: query ?? label,
     };
   }
 
